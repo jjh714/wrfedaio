5c8ebb046c3743b2d8b46417badb269ae4d6b544
jjh714 <jeonjuheon1014@gmail.com>
2026-07-20 11:21:33 +0900

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ grade = st.radio("학년", [1,2,3], horizontal=True)
 cls = st.number_input("반", value=1)
 level = st.select_slider("난이도", ["쉬움", "보통", "어려움"], value = "보통")
 score = st.slider("점수", 0, 100, 50)
-text = st.tezxt_area("소감")
+text = st.text_area("소감")
 
 if st.button("확인"):
     st.success(f"{name} / (grade)학년 / {cls}반 / {level}")
