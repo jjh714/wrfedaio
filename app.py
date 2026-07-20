7269cb2f37b98f8432a285dcf791a4bacf9fe077
jjh714 <jeonjuheon1014@gmail.com>
2026-07-20 11:28:36 +0900

    Limit grade selection to first three years

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.title("# 앱 UI 만들기")
 name =  st.text_input("nickname")
-grade = st.radio("학년", [1,2,3,4,5,6,7,8,9,10,11], horizontal=True)
+grade = st.radio("학년", [1,2,3], horizontal=True)
 cls = st.number_input("반", value=1)
 level = st.select_slider("난이도", ["쉬움", "보통", "어려움"], value = "보통")
 score = st.slider("점수", 0, 100, 50)
