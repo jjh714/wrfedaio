107dbe0c2c86e9933658cc6444229fb673a0ad40
jjh714 <jeonjuheon1014@gmail.com>
2026-07-20 10:50:41 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
+import streamlit as st
 
+st.markdown("#AI챗봇 만들기")
+st.markdown("---")
+st.markdown("##AI가 질문하면 대답합니다.")
+st.markdown("* **제작언어**: 'Python' ")
+st.markdown("* **프레임워크**: 'Streamlit' ")
+st.markdown("### '최근 대화' ")
+st.markdown("~~질문을 자유홉게 입력하세요.~~")
